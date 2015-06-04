07d42cfa1b8c475c5e308c956e7b4cb8e52b00f4
Alex Jones <alex.theboy.jones@gmail.com>
2015-06-04 11:37:58 -0700

    more functions

--- snplug.php
+++ snplug.php
@@ -61,6 +61,88 @@ class plugin_info
 	public $port; // 16 bit int;
 	public $pluginspace = array(); // 8 bit array;
 }
+
+	public function get_API_nxt64bits($obj)
+	{
+		$nxt64bits = 0;
+		$buf = array();
+		if( $obj != 0)
+		{
+			return $obj->valuedouble;
+		}
+		return calc_nxt64bits($obj);
+	}
+
+
+	public function calc_nxt64bits($NXTaddr)
+	{
+	    $nxt64bits = 0;
+	    if ( $NXTadr == 0 )
+	    {
+	        echo "calling calc_nxt64bits with null ptr!\n";
+	        return 0;
+	    }
+	    $n = strlen($NXTaddr);
+	    if ( $n >= 22 )
+	    {
+	        echo "calc_nxt64bits: illegal NXTaddr.(".$NXTaddr.") too long\n";
+	        return 0;
+	    }
+	    else if ( strcmp($NXTaddr,"0") == 0 || strcmp($NXTaddr,"false") == 0 )
+	    {
+	        // printf("zero address?\n"); getchar();
+	        return 0;
+	    }
+	    $mult = 1;
+	    $lastval = 0;
+	    for ($i=$n-1; $i>=0; $i--,$mult*=10)
+	    {
+	        $c = $NXTaddr[$i];
+	        if ( $c < '0' || $c > '9' )
+	        {
+	            echo "calc_nxt64bits: illegal char.(".$c." ".$c.") in (".$NXTaddr.").".$i."\n";
+	            return 0;
+	        }
+	        $nxt64bits += $mult * ($c - '0');
+	        if ( $nxt64bits < $lastval )
+	            echo "calc_nxt64bits: warning: 64bit overflow ".$nxt64bits." < ".$lastval."\n";
+	        $lastval = $nxt64bits;
+	    }
+	    if ( cmp_nxt64bits($NXTaddr,$nxt64bits) != 0 )
+	        echo "error calculating nxt64bits: ".$NXTaddr." -> ".$nxt64bits."\n";
+	    return $nxt64bits;
+	}
+
+
+	public function cmp_nxt64bits($str,$nxt64bits)
+	{
+	    $expanded = array();
+	    if ( $str == 0 )//|| str[0] == 0 || nxt64bits == 0 )
+	        return -1;
+	    if ( $nxt64bits == 0 && $str[0] == 0 )
+	        return 0;
+	    $expanded = expand_nxt64bits($nxt64bits);
+	    return strcmp($str,$expanded);
+	}
+
+	public function expand_nxt64bits($nxt64bits)
+	{
+	    $rev = array();
+	    $NXTaddr = array();
+	    for ($i=0; $nxt64bits!=0; i++)
+	    {
+	        $modval = $nxt64bits % 10;
+	        $rev[i] = ($char)($modval + '0');
+	        $nxt64bits /= 10;
+	    }
+	    $n = $i;
+	    for ($i=0; $i<$n; $i++)
+	        $NXTaddr[$i] = $rev[$n-1-$i];
+	    $NXTaddr[$i] = 0;
+	    return $NXTaddr;
+	}
+
+
 	
 function plugin_result($json, $tag)
 {
@@ -75,66 +157,134 @@ function plugin_result($json, $tag)
     return 0;
 }
 
-function init_pluginsocks(plugin_info $plugin, $permanentflag, $bindaddr, $connectaddr, $instanceid, $daemonid, $timeout)
-{
-	$errs = 0;
-	$socks = $plugin->all->socks;
-	if($DebugLevel > 2)
-	{
-		echo $plugin->name.".".$plugin." <<<<<<<<<<<<<<< init_permpairsocks bind.(".$bindaddr.") connect. (".$connectaddr.")\n";
-	}
-	$socks->both->pair = init_socket(".pair","pair",$NN_PAIR, 0, $connectaddr, $timeout)) < 0) $errs++;
+class Agent {
 
-	return $errs;
-}
-
-function process_json($retbuf, $max, plugin_info $plugin, $jsonargs, $initflag)
-{
-	// string
-	$filename;
-	$myipaddr;
-	$jsonstr=0;
-	// json
-	$obj = STDClass();
-	$json = STDClass();
-	// int64s
-	$allocsize;
-	$nxt64bits;
-	$tag=0;
-	if($jsonargs != 0)
+	public function init_pluginsocks(plugin_info $plugin, $permanentflag, $bindaddr, $connectaddr, $instanceid, $daemonid, $timeout)
 	{
-		$json = json_decode($jsonargs);
-		if(json_last_error() == JSON_ERROR_NONE)
+		$errs = 0;
+		$socks = $plugin->all->socks;
+		if($DebugLevel > 2)
 		{
-			if(is_array($json) && count($json) == 2)
+			echo $plugin->name.".".$plugin." <<<<<<<<<<<<<<< init_permpairsocks bind.(".$bindaddr.") connect. (".$connectaddr.")\n";
+		}
+		$socks->both->pair = init_socket(".pair","pair",$NN_PAIR, 0, $connectaddr, $timeout)) < 0) $errs++;
+
+		return $errs;
+	}
+
+	public function process_json($retbuf, $max, plugin_info $plugin, $jsonargs, $initflag)
+	{
+		// string
+		$filename;
+		$myipaddr;
+		$jsonstr=0;
+		// json
+		$obj = STDClass();
+		$json = STDClass();
+		// int64s
+		$allocsize;
+		$nxt64bits;
+		$tag=0;
+		if($jsonargs != 0)
+		{
+			$json = json_decode($jsonargs);
+			if(json_last_error() == JSON_ERROR_NONE)
 			{
-				$obj = $json[0];
-			} 
-			else $obj = $json;
-			$filename = $obj->filename;
-			if($filename[0] != 0)
-			{
-				$jsonstr = file_get_contents($filename);
-				if(strlen($jsonstr) != 0)
+				if(is_array($json) && count($json) == 2)
 				{
-					$tmp = json_decode($jsonstr);
-					if(json_last_error() == JSON_ERROR_NONE)
+					$obj = $json[0];
+				} 
+				else $obj = $json;
+				$filename = $obj->filename;
+				if($filename[0] != 0)
+				{
+					$jsonstr = file_get_contents($filename);
+					if(strlen($jsonstr) != 0)
 					{
-						$obj = $tmp;
+						$tmp = json_decode($jsonstr);
+						if(json_last_error() == JSON_ERROR_NONE)
+						{
+							$obj = $tmp;
+						}
+						else $jsonstr = 0;
 					}
-					else $jsonstr = 0;
+				}
+				if($jsonstr == 0)
+				{
+					$jsonstr = json_encode($obj);
 				}
 			}
-			if($jsonstr == 0)
+			if($obj != 0)
 			{
-				$jsonstr = json_encode($obj);
+				$nxt64bits = get_API_nxt64bits($obj->NXT);
+				if($nxt64bits != 0)
+				{
+					$plugin->nxt64bits = $nxt64bits;
+					$plugin->NXTADDR = expand_nxt64bits($plugin->nxt64bits);
+				}
+				$tag = get_API_nxt64bits($obj->tag);
+				if($initflag > 0)
+				{
+					$myidaddr = $obj->ipaddr;
+					if(is_ipaddr($myipaddr))
+					{
+						$plugin->ipaddr = $myipaddr;
+					}
+					$plugin->port = getAPI_int($obj->port), 0);
+				}
+			}
+
+			if($jsonstr != 0 && $obj != 0)
+			{
+				$retval = process_json($plugin, $tag, $retbuf, $max, $jsonstr, $obj, $initflag);
+			}
+			else echo "error with JSON.(".$jsonstr.")\n";
+			if($jsonstr != 0)
+			{
+				$jsonstr = undefined;
+			}
+			if($json != 0)
+			{
+				$json = undefined;
+			}
+			echo $retbuf;
+			return $retbuf;
+		}
+
+		public function append_stdfields($retbuf, $max, $plugin, $tag, $allfields)
+		{
+			$tagstr = array();
+			$json = json_decode($retbuf);
+			if(json_last_error() == JSON_ERROR_NONE)
+			{
+				if($tag != 0 && get_API_nxt64bits($json->tag) == 0)
+				{
+					echo ",\"tag\":\"".$tag."\n}";
+				}
+				else $tagstr[0] = 0;
+				$NXTaddr = $json->NXT;
+				if($NXTaddr == 0 || $NXTaddr[0] == 0)
+				{
+					$NXTaddr = $SUPERNET->NXTADDR;
+				}
+				echo ",\"NXT\":\"".$SUPERNET->NXTADDR."\"}";
+				if($allfields != 0)
+				{
+					if($SUPERNET->iamrelay != 0)
+					{
+						echo ",\"myipaddr\":\"".$plugin->ipaddr."\"}";
+					}
+					echo ",\"allowremote\":".$plugin->allowremote.$tagstr."\"}";
+            		echo ",\"permanentflag\":".$plugin->permanentflag.",\"myid\":\"".$plugin->myid."\",\"plugin\":\"".$plugin->name."\",\"endpoint\":\"".."\",\"millis\":".milliseconds().",\"sent\":".$plugin->numsent.",\"recv\":".$plugin->numreciv."}";
+		        }
+		        else sprintf(retbuf+strlen(retbuf)-1,",\"allowremote\":%d%s}",plugin->allowremote,tagstr);
+				}
 			}
 		}
+
+		
+
 	}
-
-	
-
-}
 
 
 ?>
